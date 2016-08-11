ca5694dfcea1f2254b9f0fd961129116711bf101
mpearmain <michael.pearmain@gmail.com>
2016-08-11 12:49:19 +0100

    autotune proximal setup

--- R/rankGeo.R
+++ R/rankGeo.R
@@ -1,13 +1,17 @@
 # Geo rank
 
 library(data.table)
-x1 <- fread("./output/ens_bag5_20160131_seed132.csv", data.table = F)
-x2 <- fread("./output/sub46.csv", data.table = F)
+x1 <- fread("./Submission.csv", data.table = F)
+x2 <- fread("./model_sub.csv", data.table = F)
 
 x1[,2] <- rank(x1[,2])/nrow(x1)
 x2[,2] <- rank(x2[,2])/nrow(x2)
 # check that the index ordering matches :-)
+x1 <- x1[with(x1, order(activity_id)), ]
+x2 <- x2[with(x2, order(activity_id)), ]
+
 xfor <- x1
-xfor[,2] <- exp(0.5 * log(x1[,2]) + 0.5 * log(x2[,2]))
+
+xfor[,2] <- exp((0.9* log(x1[,2]) + (0.1 * log(x2[,2]))))
 # save, submit, (hopefully) smile
-write.csv(xfor, "./submissions/xmix_01022016_5050.csv", row.names = F)
+write.csv(xfor, "./xmix_hacky.csv", row.names = F)
